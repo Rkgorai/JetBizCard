e345c5c86761436eb31e177b310cb801709b5dac
Rkgorai <rahul.k.gorai@gmail.com>
2024-02-11 19:31:56 +0530

    Added Different Photos and values in LazyColumn

--- app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -1,5 +1,3 @@
-package com.example.jetbizcard
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -41,21 +39,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.jetbizcard.ui.theme.JetBizCardTheme
+import com.example.jetbizcard.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetBizCardTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    CreateBizCard()
-                }
-            }
+            CreateBizCard()
         }
     }
 }
@@ -100,8 +90,6 @@ fun CreateBizCard() {
             }
 
         }
-
-
     }
 }
 
@@ -121,22 +109,37 @@ fun Content() {
             border = BorderStroke(width = 2.dp, color = Color.LightGray)
         ) {
             Portfolio(
-                data = listOf(
-                    "Project1",
-                    "Project2",
-                    "Project 3",
-                    "Project 4"
+                name = listOf(
+                    mapOf(
+                        "name" to "Anya Forger",
+                        "desc" to "Child, Telepath",
+                        "img_id" to R.drawable.anya_new
+                    ),
+                    mapOf(
+                        "name" to "Loid Forger",
+                        "desc" to "Father, Spy",
+                        "img_id" to R.drawable.loid
+                    ),
+                    mapOf(
+                        "name" to "Yor Forger",
+                        "desc" to "Mother, Assassin",
+                        "img_id" to R.drawable.yor
+                    ),
+                    mapOf(
+                        "name" to "Bond Forger",
+                        "desc" to "Dog, Can watch Future",
+                        "img_id" to R.drawable.bond
+                    )
                 )
             )
-//            Portfolio(data = listOf("Anya Forger ", "Project 2", "Project 3", "Project 4"))
         }
     }
 }
 
 @Composable
-fun Portfolio(data: List<String>) {
+fun Portfolio(name: List<Map<String, Any>>) {
     LazyColumn {
-        items(data)  { item ->
+        items(name)  { item ->
             Card(modifier = Modifier
                 .padding(13.dp)
                 .fillMaxWidth(),
@@ -148,13 +151,13 @@ fun Portfolio(data: List<String>) {
                     .padding(16.dp)
                     .fillMaxWidth()) {
 
-                    CreateImageProfile(modifier = Modifier.size(100.dp))
+                    CreateImageProfile(modifier = Modifier.size(100.dp), item["img_id"] as Int)
 
                     Column(modifier = Modifier
                         .padding(7.dp)
                         .align(alignment = Alignment.CenterVertically)) {
-                        Text(text = item, fontWeight = FontWeight.Bold)
-                        Text(text = "A great Project", style = MaterialTheme.typography.bodySmall)
+                        Text(text = item["name"].toString(), fontWeight = FontWeight.Bold)
+                        Text(text = item["desc"].toString(), style = MaterialTheme.typography.bodySmall)
                     }
 
                 }
@@ -182,7 +185,7 @@ private fun CreateInfo() {
 }
 
 @Composable
-private fun CreateImageProfile(modifier: Modifier = Modifier) {
+private fun CreateImageProfile(modifier: Modifier = Modifier, image_id: Int = R.drawable.spy_x_family) {
     Surface(
         modifier = modifier
             .size(150.dp)
@@ -194,7 +197,7 @@ private fun CreateImageProfile(modifier: Modifier = Modifier) {
         color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
     ) {
         Image(
-            painter = painterResource(id = R.drawable.anya_new),
+            painter = painterResource(id = image_id),
             contentDescription = "profile image",
             modifier = modifier.size(135.dp),
             contentScale = ContentScale.Crop
@@ -203,11 +206,8 @@ private fun CreateImageProfile(modifier: Modifier = Modifier) {
     }
 }
 
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    JetBizCardTheme {
-        CreateBizCard()
-    }
+    CreateBizCard()
 }
